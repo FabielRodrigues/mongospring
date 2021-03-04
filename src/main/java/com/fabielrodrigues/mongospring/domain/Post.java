d0e008ebc404d98abeada8703810a43714d92de7
Fabiel Rodrigues <fabiel.rodrigues@gmail.com>
2021-03-04 11:19:41 -0300

    Consulta simples com Query Methods

--- src/main/java/com/fabielrodrigues/mongospring/domain/Post.java
+++ src/main/java/com/fabielrodrigues/mongospring/domain/Post.java
@@ -1,12 +1,15 @@
 package com.fabielrodrigues.mongospring.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fabielrodrigues.mongospring.dto.AuthorDTO;
+import com.fabielrodrigues.mongospring.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -21,6 +24,7 @@ public class Post implements Serializable {
 	private String body;
 	private AuthorDTO author;
 	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -83,6 +87,15 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 
---
+++ src/main/java/com/fabielrodrigues/mongospring/dto/CommentDTO.java
@@ -0,0 +1,48 @@
+package com.fabielrodrigues.mongospring.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class CommentDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String text;
+	private Date date;
+	private AuthorDTO author;
+	
+	public CommentDTO(String string, Date date2, AuthorDTO authorDTO) {
+		
+	}
+	
+	public CommentDTO(CommentDTO obj) {
+		text = obj.getText();
+		date = obj.getDate();
+		author = obj.getAuthor();
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public AuthorDTO getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(AuthorDTO author) {
+		this.author = author;
+	}
+	
+	
+}
