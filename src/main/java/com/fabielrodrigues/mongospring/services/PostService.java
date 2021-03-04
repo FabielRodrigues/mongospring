0f5169ad568fc66f219c9ef58a6a90ad752c0ee0
Fabiel Rodrigues <fabiel.rodrigues@gmail.com>
2021-03-04 09:25:53 -0300

    Obtendo post por id

---
+++ src/main/java/com/fabielrodrigues/mongospring/services/PostService.java
@@ -0,0 +1,25 @@
+package com.fabielrodrigues.mongospring.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fabielrodrigues.mongospring.domain.Post;
+import com.fabielrodrigues.mongospring.repository.PostRepository;
+import com.fabielrodrigues.mongospring.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Post post = repo.findById(id).orElse(null);
+		if (post == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado!");
+		}
+		return post;
+	}	
+	
+}
